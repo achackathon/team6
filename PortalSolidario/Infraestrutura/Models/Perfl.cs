4deefbeaeff4f31ff2b6b16dc36cb717c67da02b
Geeh <geeh@fumec.edu.br>
2016-03-05 10:05:01 -0300

    classe perfil e usuario

---
+++ PortalSolidario/Infraestrutura/Models/Perfl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Models
+{
+    public class Perfl
+    {
+        public int Id_pergil { get; set; }
+        public int CodigoPerfil { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ PortalSolidario/Infraestrutura/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Models
+{
+    public class Usuario
+    {
+        public int id_Usuario { get; set; }
+        public Perfl Perfil { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cep { get; set; }
+        public string Documento { get; set; }
+    }
+}
